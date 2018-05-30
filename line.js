2f6680c94f1f7ac7a4ecc1c6a80d2235f6c77f26
Vishnuparammal <vishnu.130599@gmail.com>
2018-05-30 14:39:26 +0530

    line,mirror and lens with focal length and angles

--- line.js
+++ line.js
@@ -20,9 +20,16 @@ function mirrorFlag()
 	flag=2;
 	n=1;
 }
+
+function lensFlag()
+{
+	flag=3;
+	n=1;
+}
+
 function clickCheck()
 {
-	if (flag==1||flag==2)
+	if (flag)
 	{
 		click=1;
 		
@@ -61,8 +68,8 @@ function draw()
 		if (n==1)
 		{
 			roc=2*prompt("enter focal length");
-			a1 = -PI/180*prompt("enter angle above pole");
-			a2 = PI/180*prompt("enter angle below pole ");
+			a1 = -PI/180*prompt("enter angle");
+			a2 = -a1;
 			n=0;
 		}
 		
@@ -71,13 +78,45 @@ function draw()
 			
 			x[0]=mouseX;
 			y[0]=mouseY;
+			x[1]=x[0]-roc/2;
+			y[1]=y[0]
 			noFill();
-			arc(x[0]-roc/2,y[0],roc,roc,a1,a2);
+			arc(x[1],y[1],roc,roc,a1,a2);
 			click=0;
 			flag=0;
 		
 		}
 		
+	}
+	
+	if (flag==3)
+	{
+		
+		if (n==1)
+		{
+			roc=2*prompt("enter focal length");
+			a1 = -PI/180*prompt("enter angle");
+			a2 = -a1;
+			a3 = PI+a1;
+			a4 = PI+a2;
+			n=0;
+		}
+		
+		if (click==1)
+		{
+			
+			x[0]=mouseX
+			y[0]=mouseY;
+			x[1]=x[0]-roc*cos(a1)/2;
+			y[1]=y[0];
+			x[2]=x[0]+roc*cos(a1)/2;
+			y[2]=y[0];
+			arc(x[1],y[1],roc,roc,a1,a2,OPEN);
+			arc(x[2],y[2],roc,roc,a3,a4,OPEN);
+			click=0;
+			flag=0;
+		
+		}	
 	}
 	
 
